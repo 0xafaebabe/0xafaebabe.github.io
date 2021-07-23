7eefb54269d766fa9ed385bd167f98d9a2719e39
nixie design studio <app@nixiedesignstudio.com>
2021-07-23 11:27:48 -0700

    migrated portfolio previews

--- _includes/Footer.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="text-center">Hand-designed and lovingly coded in vue.js by Kylie Jack. &copy;2020</div>
-</template>
-
-<script>
-export default {
-  name: "Footer"
-};
-</script>
